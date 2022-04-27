12e09dd169e34ffff7b16441e91eabf925c08cd9
David Dombrowsky <davek@6thstreetradio.org>
2022-04-26 23:43:40 -0400

    updated index.html to include both address options

--- sql/schema.sql
+++ sql/schema.sql
@@ -26,7 +26,7 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.account (
     memo character varying,
-    ftm_address character varying,
+    address character varying,
     amount real
 );
 
